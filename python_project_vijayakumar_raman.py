5fb5eea763863e4a5ac45d7843d1cfa79176795b
VijayakumarRaman <149077507+VijayakumarRaman@users.noreply.github.com>
2023-10-27 15:58:52 +0530

    Movier picker

--- python_project_vijayakumar_raman.py
+++ python_project_vijayakumar_raman.py
@@ -1,3 +1,4 @@
+# Movie picker project made by Vijayakumar Raman
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -5,11 +6,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pandas as pd
-import urllib.parse
 import random
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.common.exceptions import WebDriverException
+from time import sleep
 
 driver = None
+user_choice = ''
+choice = ''
+movies = []
+home = '//div[3]/a[@target="_self"]'
+movie_xpath = '//article/div/a[@target="_self"]'
+search = '//input[@id="searchBar"]'
+error = 0
+
 def install():
     global driver
     try:
@@ -17,69 +26,64 @@ def install():
         chrome_options.add_argument("--headless")
         driver_service = webdriver.chrome.service.Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=driver_service, options=chrome_options)
+        driver.maximize_window()
     except:
         install()
-install()
 
-genres_links = []
-genres_title = []
-def get_genre():
+def user():
+    global choice, user_choice
+    while not user_choice:
+        user_choice = input("Please enter your choice of Genre: ").lower()
+    choice = user_choice +' movies'
+
+def get_movie():
+    global movies, user_choice, error
+
     try:
-        driver.get('https://tubitv.com/home')
-        wait = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//button[@class="XVtXO qmpj2"]')))
-        driver.find_element(By.XPATH, '//button[@class="XVtXO qmpj2"]').click()
-        wait = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.XPATH, '//div[@class="Button__content"]')))
-        genres_xpath = driver.find_elements(By.XPATH, '//*[@id="app"]/div[3]/div/div[1]/div/div[2]/div[1]/a')
-        for genre in genres_xpath:
-            genres_links.append(genre.get_attribute('href'))
+        driver.get('https://www.hotstar.com/in/explore')
+        wait = WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.XPATH, home)))
 
-        for genre in genres_links:
-            genres_title.append(genre[28:])
+        driver.find_element(By.XPATH, search).send_keys(choice)
+        sleep(3)
+        titles = driver.find_elements(By.XPATH, movie_xpath)
+        movies_link = []
 
-        df = pd.DataFrame({'Genres': genres_title, 'Links': genres_links})
-        print('Please find the list of Genres available on the Tubi TV!')
-        print(df['Genres'].to_string(index=False))
+        for title in titles:
+            movies_link.append(title.get_attribute('href'))
+
+        for movie in movies_link:
+            if 'https://www.hotstar.com/in/movies/' in movie:
+                movies.append(movie[34:-11])
+
+        df = pd.DataFrame({'Movies': movies})
+        df['Movies'] = df['Movies'].str.replace('-', ' ')
+        count = len(movies)
+        print(f'\nWe found {count} movies for', user_choice)
+        print('Please find the movie list below')
+        print(df['Movies'].to_string(index=False))
+
+        movies = df['Movies'].to_list()
+
+        print('\nI would recommend you to watch:', random.choice(movies))
+        print('\nHope you will enjoy the movie!')
     
-    except:
-        get_genre()
-
-get_genre()
-
-movies = []
-def find():
-    user_choice = input("Please enter your choice of Genre: ").lower()
-    try:
-        if user_choice in genres_title:
-            driver.get(f'https://tubitv.com/category/{user_choice}')
-            wait = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '//div/a[@class="web-content-tile__title"]')))
-            movies_xpath = driver.find_elements(By.XPATH, '//div/a[@class="web-content-tile__title"]')
-            for movie in movies_xpath:
-                movies.append(movie.text)
-            print(f'we found {len(movies)} movies from', user_choice)
-            print(f'Please find a random movie from {user_choice}:', random.choice(movies))
-            print('\nHope you will enjoy the movie!')
-        else:
-            print('\n The given choice of genre is not listed on the above so, using search key with your choice to assist you better')
-            user = urllib.parse.quote(user_choice)
-            driver.get(f'https://tubitv.com/search/{user}')
-            wait = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '//div/a[@class="web-content-tile__title"]')))
-            movies_xpath = driver.find_elements(By.XPATH, '//div/a[@class="web-content-tile__title"]')
-            for movie in movies_xpath:
-                movies.append(movie.text)
-            print(f'we found {len(movies)} movies from', user_choice)
-            print(f'Please find a random movie from {user_choice}:', random.choice(movies))
-            print('\nHope you will enjoy the movie!')
-    except NoSuchElementException:
-        print("Element not found or No movies found, please try with diffrent choice")
-        find()
-
     except WebDriverException:
-        print("An error occurred:", 'TimeoutException, could not find any result, please try with different choice')
-        find()
+        error = error+1
+        print('\nAn error occurd on the website, please wait we will reload the page')
+        if error<4:
+            get_movie()
+        else:
+            print('\nmaximum of 3 attempts were failed, please restart the program')
+            sleep(5)
+    except:
+        print('\nNo results found, please try again with different choice of genre')
+        user_choice = ''
+        user()
+        get_movie()
 
-find()
+
+install()
+user()
+get_movie()
 driver.quit()
